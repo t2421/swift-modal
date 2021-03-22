e70a2cece06e8770d9418a9cf260cac243a87b11
Taiga Kiyotaki <t2421gm@gmail.com>
2021-03-23 08:40:30 +0900

    Initial Commit

---
+++ modal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  modal
+//
+//  Created by tech-prty on 2021/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
